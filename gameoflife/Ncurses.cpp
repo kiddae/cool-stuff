5c59bf10d2402c4197ceaba107bcfcf4362f0714
Louise Nguyen <nguyenlelouise@gmail.com>
2021-10-31 01:02:21 +0200

    Add Ncurses version and all-in-one (bad code don't look...)

--- gameoflife/Ncurses.cpp
+++ gameoflife/Ncurses.cpp
@@ -2,21 +2,13 @@
 #include <vector>
 #include <thread>
 #include <stdlib.h>
-#include <time.h>
-
-#ifdef _WIN32 | _WIN64
-#include <Windows.h>
-
-#else
 #include <ncurses.h>
-
-#endif
+#include <time.h>
 
 using namespace std;
 
 void InitArray(vector<vector<int>>& vGrid, int nHeight, int nWidth, bool bRandom = true)
 {
-    srand(time(NULL));
     vGrid = vector<vector<int>>(nHeight,vector<int>(nWidth,0));
     int nMargin = 4;
     if (bRandom)
@@ -36,72 +28,39 @@ void ResizeGrid(vector<vector<int>>& vGrid, int nWidth, int nHeight)
     vGrid.resize(nHeight, vector<int>(nWidth, 0));
 }
 
-#ifdef _WIN32 | _WIN64
-void GetScreenSize(HANDLE hConsole, int& nHeight, int& nWidth)
-{
-    CONSOLE_SCREEN_BUFFER_INFO *pConsoleInfo = new CONSOLE_SCREEN_BUFFER_INFO;
-    GetConsoleScreenBufferInfo(hConsole, pConsoleInfo);
-    nWidth = abs(pConsoleInfo->srWindow.Right - pConsoleInfo->srWindow.Left) + 1;
-    nHeight = abs(pConsoleInfo->srWindow.Bottom - pConsoleInfo->srWindow.Top) + 1;
-}
-#else
-void GetScreenSize(int& nHeight, int& nWidth) { getmaxyx(stdscr, nHeight, nWidth); }
-#endif
-
 
 int main()
 {
 
 
     // ========== Set up graphics ========== //
-#ifdef _WIN64 | _WIN32
-    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_WRITE | GENERIC_READ, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-    SetConsoleActiveScreenBuffer(hConsole); 
-    DWORD dBytesWritten = 0;
-#else
     initscr();
     cbreak();
     noecho();
-#endif
+    keypad(stdscr, true);
 
     // Initialize grid and get screen size
-    int nHeight, nWidth;
-#ifdef _WIN64 | WIN32
-    GetScreenSize(hConsole, nHeight, nWidth);
-#else
-    GetScreenSize(nHeight, nWidth);
-#endif
-
+    int nWidth = getmaxx(stdscr);
+    int nHeight = getmaxy(stdscr);
     vector<vector<int>> vGrid(nHeight, vector<int>(nWidth));
-
-    // ========== Setting up the grid ========== //
+    srand(time(NULL));
     InitArray(vGrid, nHeight, nWidth);
     
 
+    // ========== Setting up the grid ========== //
 
     // ========== Game loop ========== //
     while (1)
     {
 
         // ========== Input ========== //
-#ifdef _WIN32 | _WIN64
-        if (GetAsyncKeyState(VK_SPACE)) InitArray(vGrid, nHeight, nWidth);
-#else
-        if (getch() == ' ') InitArray(vGrid, nHeight, nWidth);
-#endif
+        if (getch() == ' ')
+            InitArray(vGrid, nHeight, nWidth);
 
         // ========== Compute ========== //
         // Resize grid according to screen size.
-#ifdef _WIN64 | WIN32
-        GetScreenSize(hConsole, nHeight, nWidth);
-#else
-        GetScreenSize(nHeight, nWidth);
-#endif
+        getmaxyx(stdscr,nHeight, nWidth);
         ResizeGrid(vGrid, nWidth, nHeight);
-#ifdef _WIN64 | WIN32
-        // Make sure the buffer size is the same as the screen. 
-        SetConsoleScreenBufferSize(hConsole, {short(nWidth), short(nHeight)}); 
-#endif
 
         // The Game of Life!
         vector<vector<int>> vNext(nHeight, vector<int>(nWidth,0));
@@ -140,20 +99,18 @@ int main()
         // cDead = 0x2591;
         cAlive = 0x2588;
         cDead = ' ';
-#ifdef _WIN32 | _WIN64
-        wchar_t *pScreen = new wchar_t[nWidth * nHeight];
         for (int y = 0; y < nHeight; y++)
+        {
             for (int x = 0; x < nWidth; x++)
-                pScreen[y * nWidth + x] = (vGrid[y][x] == 1) ? cAlive : cDead;
-        WriteConsoleOutputCharacterW(hConsole, pScreen, nWidth * nHeight, {0, 0}, &dBytesWritten);
-#else
-        for (int y = 0; y < nHeight; y++)
-            for (int x = 0; x < nWidth; x++)
-                mvaddch(y, x, (vGrid[y][x] == 1) ? cAlive : cDead);
-#endif
+            {
+                mvaddch(y, x, (vGrid[y][x] == 1? cAlive : cDead));
+            }
+        }
+        refresh();
 
         // ========== Timing ========== //
         this_thread::sleep_for(50ms);
     }
+    endwin();
     return 0;
 }
--- gameoflife/Windows.cpp
+++ gameoflife/Windows.cpp
@@ -2,21 +2,13 @@
 #include <vector>
 #include <thread>
 #include <stdlib.h>
-#include <time.h>
-
-#ifdef _WIN32 | _WIN64
 #include <Windows.h>
-
-#else
-#include <ncurses.h>
-
-#endif
+#include <time.h>
 
 using namespace std;
 
 void InitArray(vector<vector<int>>& vGrid, int nHeight, int nWidth, bool bRandom = true)
 {
-    srand(time(NULL));
     vGrid = vector<vector<int>>(nHeight,vector<int>(nWidth,0));
     int nMargin = 4;
     if (bRandom)
@@ -25,6 +17,20 @@ void InitArray(vector<vector<int>>& vGrid, int nHeight, int nWidth, bool bRandom
             for (int x = nWidth / 2 - nMargin; x < nWidth / 2 + nMargin; x++)
                 vGrid[y][x] = rand()%2;
     }
+}
+
+int GetScreenSizeX(HANDLE hConsole)
+{
+    CONSOLE_SCREEN_BUFFER_INFO *pConsoleInfo = new CONSOLE_SCREEN_BUFFER_INFO;
+    GetConsoleScreenBufferInfo(hConsole, pConsoleInfo);
+    return abs(pConsoleInfo->srWindow.Right - pConsoleInfo->srWindow.Left) + 1;
+}
+
+int GetScreenSizeY(HANDLE hConsole)
+{
+    CONSOLE_SCREEN_BUFFER_INFO *pConsoleInfo = new CONSOLE_SCREEN_BUFFER_INFO;
+    GetConsoleScreenBufferInfo(hConsole, pConsoleInfo);
+    return abs(pConsoleInfo->srWindow.Bottom - pConsoleInfo->srWindow.Top) + 1;
 }
 
 void ResizeGrid(vector<vector<int>>& vGrid, int nWidth, int nHeight)
@@ -36,72 +42,41 @@ void ResizeGrid(vector<vector<int>>& vGrid, int nWidth, int nHeight)
     vGrid.resize(nHeight, vector<int>(nWidth, 0));
 }
 
-#ifdef _WIN32 | _WIN64
-void GetScreenSize(HANDLE hConsole, int& nHeight, int& nWidth)
-{
-    CONSOLE_SCREEN_BUFFER_INFO *pConsoleInfo = new CONSOLE_SCREEN_BUFFER_INFO;
-    GetConsoleScreenBufferInfo(hConsole, pConsoleInfo);
-    nWidth = abs(pConsoleInfo->srWindow.Right - pConsoleInfo->srWindow.Left) + 1;
-    nHeight = abs(pConsoleInfo->srWindow.Bottom - pConsoleInfo->srWindow.Top) + 1;
-}
-#else
-void GetScreenSize(int& nHeight, int& nWidth) { getmaxyx(stdscr, nHeight, nWidth); }
-#endif
-
 
 int main()
 {
 
 
     // ========== Set up graphics ========== //
-#ifdef _WIN64 | _WIN32
     HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_WRITE | GENERIC_READ, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
     SetConsoleActiveScreenBuffer(hConsole); 
     DWORD dBytesWritten = 0;
-#else
-    initscr();
-    cbreak();
-    noecho();
-#endif
 
     // Initialize grid and get screen size
-    int nHeight, nWidth;
-#ifdef _WIN64 | WIN32
-    GetScreenSize(hConsole, nHeight, nWidth);
-#else
-    GetScreenSize(nHeight, nWidth);
-#endif
-
+    int nWidth = GetScreenSizeX(hConsole);
+    int nHeight = GetScreenSizeY(hConsole);
     vector<vector<int>> vGrid(nHeight, vector<int>(nWidth));
-
-    // ========== Setting up the grid ========== //
+    srand(time(NULL));
     InitArray(vGrid, nHeight, nWidth);
     
 
+    // ========== Setting up the grid ========== //
 
     // ========== Game loop ========== //
     while (1)
     {
 
         // ========== Input ========== //
-#ifdef _WIN32 | _WIN64
-        if (GetAsyncKeyState(VK_SPACE)) InitArray(vGrid, nHeight, nWidth);
-#else
-        if (getch() == ' ') InitArray(vGrid, nHeight, nWidth);
-#endif
+        if (GetAsyncKeyState(VK_SPACE))
+            InitArray(vGrid, nHeight, nWidth);
 
         // ========== Compute ========== //
         // Resize grid according to screen size.
-#ifdef _WIN64 | WIN32
-        GetScreenSize(hConsole, nHeight, nWidth);
-#else
-        GetScreenSize(nHeight, nWidth);
-#endif
+        nWidth = GetScreenSizeX(hConsole);
+        nHeight = GetScreenSizeY(hConsole);
         ResizeGrid(vGrid, nWidth, nHeight);
-#ifdef _WIN64 | WIN32
         // Make sure the buffer size is the same as the screen. 
         SetConsoleScreenBufferSize(hConsole, {short(nWidth), short(nHeight)}); 
-#endif
 
         // The Game of Life!
         vector<vector<int>> vNext(nHeight, vector<int>(nWidth,0));
@@ -134,23 +109,21 @@ int main()
         vGrid = vNext;
 
         // ========== Render ========== //
+        wchar_t *pScreen = new wchar_t[nWidth * nHeight];
         wchar_t cAlive;
         wchar_t cDead;
         // cAlive = 0x2588;
         // cDead = 0x2591;
         cAlive = 0x2588;
         cDead = ' ';
-#ifdef _WIN32 | _WIN64
-        wchar_t *pScreen = new wchar_t[nWidth * nHeight];
         for (int y = 0; y < nHeight; y++)
+        {
             for (int x = 0; x < nWidth; x++)
+            {
                 pScreen[y * nWidth + x] = (vGrid[y][x] == 1) ? cAlive : cDead;
+            }
+        }
         WriteConsoleOutputCharacterW(hConsole, pScreen, nWidth * nHeight, {0, 0}, &dBytesWritten);
-#else
-        for (int y = 0; y < nHeight; y++)
-            for (int x = 0; x < nWidth; x++)
-                mvaddch(y, x, (vGrid[y][x] == 1) ? cAlive : cDead);
-#endif
 
         // ========== Timing ========== //
         this_thread::sleep_for(50ms);
